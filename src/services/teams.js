049204e289a9db2778d21ec33261e95ca726c4d2
Abraham Serrano <abrahamserrano96@hotmail.com>
2023-02-05 17:10:40 -0600

    fix: change api midu for api abraham

--- src/services/teams.js
+++ src/services/teams.js
@@ -1,7 +1,9 @@
 export const getAllTeams = async () => {
   try {
-    const response = await fetch('https://api.kingsleague.dev/teams')
-    const teams = await (await response).json()
+    const response = await fetch(
+      'https://kings-league-api-abrahamsm.abrahamserrano96.workers.dev/teams'
+    )
+    const teams = await response.json()
 
     return teams
   } catch (error) {
